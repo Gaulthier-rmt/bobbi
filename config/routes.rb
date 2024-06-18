4759b1325eebed818fb69da5010de40b0d1666f0
Gaulthier Remusat <gaulthier.remusat@kedgebs.com>
2024-06-18 10:25:23 +0200

    setup heroku, début routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :groupes do
+    member do
+      get :galerie
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :evenements do
+    member do
+      get :recap
+      get :liste
+      resources :photos, only: [:create, :destroy]
+    end
+  end
 end

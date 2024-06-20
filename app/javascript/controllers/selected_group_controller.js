732be3fd427374a66553c347a5cbc0251b40131f
Romane Arvois <romane.arvois@kedgebs.com>
2024-06-20 16:33:56 +0200

    group show + index

---
+++ app/javascript/controllers/selected_group_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="selected-group"
+export default class extends Controller {
+
+  static values = { groupId: Number }
+
+  static targets = [ "group" ]
+
+  connect() {
+    console.log("Hello, Stimulus!")
+    console.log(this.selectedGroupTarget);
+  }
+
+  update(event) {
+    const id = event.target.dataset.selectedGroupGroupIdValue
+    console.log(id);
+    const url = `/groups?group_id=${id}`
+    fetch(url, {headers: { "Accept": "text/plain" }})
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+        this.groupTarget.outerHTML = data
+      })
+  }
+}
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,4 @@ class Group < ApplicationRecord
   has_many :events, through: :event_groups
 
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
 end

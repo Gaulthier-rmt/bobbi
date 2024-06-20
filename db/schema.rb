fc830bde5066d94f310b1dd20b0454911863a0c4
Gaulthier Remusat <gaulthier.remusat@kedgebs.com>
2024-06-20 17:57:07 +0200

    oups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_100933) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_111623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_100933) do
   create_table "event_categories", force: :cascade do |t|
     t.bigint "event_id", null: false
     t.bigint "category_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_event_categories_on_category_id"
